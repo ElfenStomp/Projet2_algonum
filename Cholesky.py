6c66ba55037f96d15dda713f678e19985a83ddeb
Elfen <xav.tps@hotmail.fr>
2017-02-13 11:20:52 +0200

    matrice_creation

--- Cholesky.py
+++ Cholesky.py
@@ -1,2 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+
+def display(t):
+    for i in range(0, len(t[0]), 1):
+        print(t[i])
+    
+def matrice_creation(n):
+    m = n**2
+    t=np.zeros([m, m])
+    for i in range(0, m, 1):
+        t[i][i] = -4        #diagonale
+        if i%n != 0:   
+            t[i][i-1] = 1   #extra diagonale inferieur
+            t[i-1][i] = 1   #extra diagonale supérieur
+        if (i >= n):
+            t[i-n][i] = 1   #diagonale supérieur
+            t[i][i-n] = 1   #diagonale inférieur
+        
+            
+    display(t)
+    
+matrice_creation(4)
+            
