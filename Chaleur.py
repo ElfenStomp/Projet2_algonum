e0e334e5c537cc227394d4f711cb5acba037ad95
Elfen <xav.tps@hotmail.fr>
2017-02-14 12:42:41 +0200

    Chaleur.py

--- Chaleur.py
+++ Chaleur.py
@@ -10,19 +10,27 @@ def display(M):
     print("_____________________________")
 
 ##
-def matrice_chaleur(n):
-    m = n**2
+def matrice_chaleur(N):
+    m = N**2
     A = np.zeros([m, m])
     for i in range(0, m, 1):
         A[i][i] = -4        #diagonale
-        if i%n != 0:   
+        if i%N != 0:   
             A[i][i-1] = 1   #extra diagonale inferieur
             A[i-1][i] = 1   #extra diagonale supérieur
-        if (i >= n):
-            A[i-n][i] = 1   #diagonale supérieur
-            A[i][i-n] = 1   #diagonale inférieur
+        if (i >= N):
+            A[i-N][i] = 1   #diagonale supérieur
+            A[i][i-N] = 1   #diagonale inférieur
     return A
 
+##
+def creation_b(N):
+    b = np.zeros([N**2, 1])
+    return b 
+
 ##BEGIN
-A = matrice_chaleur(2)
+N = 2
+A = matrice_chaleur(N)
 display(A)
+b = creation_b(N)
+h = 1 / (N + 1)
