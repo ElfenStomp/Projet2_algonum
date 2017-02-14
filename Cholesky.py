32b4b3a0c2184cc6bd9effd378d0799731149efb
Elfen <xav.tps@hotmail.fr>
2017-02-14 08:09:55 +0200

    Cholesky

--- Cholesky.py
+++ Cholesky.py
@@ -15,7 +15,7 @@ def transposer_triangulaire_inf(L):
     for i in range(0, m, 1):
         for j in range(0, i+1, 1):
             L_t[j][i] = L[i][j]
-    display(L_t)
+    return L_t
 #Pas besoin de calculer L_t (prend de la place mémoire inutile)
 
 ##Cholesky
@@ -30,12 +30,27 @@ def cholesky(A):
             for k in range(0, i, 1):
                 L[j][i] += L[i][k]*L[j][k]
             L[j][i] = (A[i][j] - L[j][i]) / L[i][i]
-    display(L)
-    transposer_triangulaire_inf(L)
+    return L
 #Complexité: (n**3)/6 additions et multiplications, (n*(n-1))/2 divisions, n évaluations de racines carrées
 
+##Cholesky imcomplet
+def cholesky_imcomplet(A):
+    m = len(A[0])
+    L = np.zeros([m, m])
+    for i in range(0, m, 1):      
+        if A[i][i] != 0:
+            for k in range(0, i, 1):
+                L[i][i] += L[i][k]**2
+            L[i][i] = math.sqrt(A[i][i] - L[i][i])
+        for j in range(i+1, m, 1): 
+            if A[j][i] != 0:
+                for k in range(0, i, 1):
+                    L[j][i] += L[i][k]*L[j][k]
+                L[j][i] = (A[i][j] - L[j][i]) / L[i][i]
+    return L 
+    
 ##
-def matrice_creation(n):
+def matrice_chaleur(n):
     m = n**2
     A = np.zeros([m, m])
     for i in range(0, m, 1):
@@ -47,10 +62,19 @@ def matrice_creation(n):
             A[i-n][i] = 1   #diagonale supérieur
             A[i][i-n] = 1   #diagonale inférieur
     display(A)
-    
-matrice_creation(2)
+
+##
+def preconditionneur(A):
+    L = cholesky(A)
+    display(L)
+    L_t = transposer_triangulaire_inf(L)
+    display(L_t) 
+    print(np.linalg.cond(A))
+    print(np.linalg.cond(np.dot(np.linalg.inv(np.dot(L, L_t)), A)))
+
+matrice_chaleur(2)
 A=np.array([[1, 1, 1, 1], [1, 5, 5, 5], [1, 5, 14, 14], [1, 5, 14, 15]])
-cholesky(A)
+preconditionneur(A)
 
 ##Partie 1 
 """
@@ -59,5 +83,7 @@ cholesky(A)
    L * L_t * x = b      n**3
    -> L * y = b         n**2
    -> L_t * x = y       n**2
+   
+   Au final la compléxité est de l'ordre de O(n**3)
 """
 ## 
