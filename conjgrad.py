299655404b439d986183fa1dbc20fc59f35e39d3
polochinoc <polochinoc@gmail.com>
2017-03-09 19:30:47 +0100

    Gradient Conjugué

--- conjgrad.py
+++ conjgrad.py
@@ -7,6 +7,7 @@ import math as m
 import scipy.linalg as spl # pour la fonction solve, qui résoud une équation de type AX = B
 import matplotlib.pyplot as plt # pour tracer la courbe de comparaisons entre la méthode du gradient conjugué
                                 # et la fonction solve de la bibliothèque scipy.linalg
+import sdp_matrix as sdp # pour générer des matrices symétriques définies positives à diagonale dominante
 
 # 1
 """
@@ -14,19 +15,14 @@ import matplotlib.pyplot as plt # pour tracer la courbe de comparaisons entre la
 _ while ( i < imax && sqrt(rsold) > 10**(-10)) et on ajoute la ligne d'incrémentation i += 1 dans
 la boucle. Avec imax, un nombre d'itérations maximal de l'orde de 10^3. De telle sorte qu'on peut enlever
 le test if dans la boucle for et remplacer cette boucle for par une boucle while.
-
 _ Le vecteur solution X que l'on recherche est un argument de la fonction, il peut donc être très
 éloigné de la valeur à laquelle on veut aboutir. Cependant, cela ralentit-il vraiment la convergence de r vers 0
 i.e de AX vers B ?
-
 _ Le produit entre A et X est matriciel, il serait judicieux de le préciser par une fonction
 intermédiaire, de même que r'*r est un produit scalaire entre 2 vecteurs.
-
 _ r' est le vecteur transposé de r, cela n'est pourtant pas clair dans le programme.
-
 _ C'est également le cas des opérations +/- sur des vecteurs/matrices, qui doivent être implémentées correctement
 en Python.
-
 _ La fonction ne retourne rien.
 """
 
@@ -38,7 +34,15 @@ Or inverse(T*T') = inverse(T') * inverse(T), toutes les 2 inversibles car sinon 
 X = inverse(T') * inverse(T) * B.
 
 La méthode du gradient conjugué fait tendre X vers le vecteur solution en faisant tendre
-R = B - AX vers le vecteur nul.
+R = B - AX vers le vecteur nul. Pour ce qui est de la complexité, celle-ci est en imax*0(n²). En effet, on ne fait jamais
+de produit matriciel entre 2 matrices carrées de taille n x n mais entre une matrice carrée de dimension n
+et un vecteur de taille n. Avec un nombre imax d'itération constant dans la boucle.
+
+Or Choleski est en O(n^3), on gagne donc un facteur n. Si on veut calculer le gain de complexité par rapport
+à N = n², on a une complexité linéaire par rapport à N pour la méthode du gradient conjugué et en O(N^(3/2)) pour Choleski.
+
+Pour n faible, on a n^3 < imax * n². Donc pour n < imax, la complexité de Choleski est meilleure que celle-ci
+du gradient conjugué.
 
 (à  voir avec ceux qui traitent la 1ère partie pour répondre à cette question)
 """
@@ -62,49 +66,57 @@ def is_symdefpos(M):
 
     return True
 
-def conjgrad_2(A,B,X,imax,p):
+def conjgrad(A,B,X,imax,p):
     if (is_symdefpos(A) == False):
         # On vérifie que A soit bien symétrique définie positive.
         print("\n A n'est pas symétrique définie positive")
-        return np.zeros((np.shape(A)[0],0))
+        return np.zeros((np.shape(A)[0],1))
     R = B - A.dot(X)
     P = R
     rs_old = np.transpose(R).dot(R)
     for i in range(1, imax + 1):
         Ap = A.dot(P)
         alpha = rs_old / np.transpose(P).dot(Ap)
-        #print("\n X = \n", X)
         X = X + (alpha * P)
-        #print("\n R = \n", R)
         R = R - (alpha * Ap)
         rs_new = np.transpose(R).dot(R)
-        if (rs_new < p):
+        if (m.sqrt(rs_new) < p):
             break
         P = R + (rs_new/rs_old) * P
         rs_old = rs_new
+    print("\n R = \n", R)
+    print("\n X = \n", X)
     return X
 
-def conjgrad(A,B,X,imax,p):
+def conjgrad_precond(A,B,X,imax,p):
     if (is_symdefpos(A) == False):
         # On vérifie que A soit bien symétrique définie positive.
         print("\n A n'est pas symétrique définie positive")
-        return np.zeros((np.shape(A)[0],0))
+        return np.zeros((np.shape(A)[0],1))
     R = B - A.dot(X)
-    P = R
-    rs_old = np.transpose(R).dot(R)
-    i = 0
-    while (i < imax and rs_old >= p):
+    #M = matrix_inv_approx(A)
+    M = spl.inv(A)
+    Z = M.dot(R)
+    P = Z
+    rs_old = np.transpose(R).dot(Z)
+    rs_old_beta = np.transpose(Z).dot(R)
+    for i in range(1, imax + 1):
         Ap = A.dot(P)
         alpha = rs_old / np.transpose(P).dot(Ap)
         X = X + (alpha * P)
-        print("\n X = \n", X)
         R = R - (alpha * Ap)
-        print("\n R = \n", R)
-        rs_new = np.transpose(R).dot(R)
-        P = R + (rs_new/rs_old) * P
+        if (m.sqrt(np.transpose(R).dot(R)) < p):
+            break
+        Z = M.dot(R)
+        rs_new = np.transpose(R).dot(Z)
+        rs_new_beta = np.transpose(Z).dot(R)
+        P = Z + (rs_new_beta/rs_old_beta) * P
         rs_old = rs_new
-        i += 1
+        rs_old_beta = rs_new_beta
+    print("\n R = \n", R)
+    print("\n X = \n", X)
     return X
+
 
 def tst_conjgrad():
         # Initialisation des arguments de conjgrad
@@ -115,48 +127,50 @@ def tst_conjgrad():
     print("B = \n", B, "\n Dimension de B: ", np.shape(B))
     X = np.array([[0.],[0.],[0.]]) # On initialise X au vecteur nul de R^3
     imax = 10**3
-    p = 10**(-10)
+    p = 10**(-100)
     print("\n Précision à ",p," près \n")
         # Comparaison sommaire entre conjgrad et np.linalg.solve
-    X1 = conjgrad_2(A, B, X, imax, p)
+    X1 = conjgrad_precond(A, B, X, imax, p)
     X2 = spl.solve(A, B)
     print("\n Avec la méthode du gradient conjugué on a X = \n", X1)
     print("Avec la fonction solve de la biliothèque scipy.linalg on a X = \n", X2)
 
-    while True :
-        answer = int(input("Ecrivez 1 si vous souhaitez continuer le programme pour afficher les courbes de convergence de X en fonction de la précision ou 0 si vous souhaitez arrêter le programme: "))
-        if answer == 1 :
-            print("Suite du programme")
-            break
-        elif answer == 0 :
-            print("Arrêt du programme de test")
-            return
-        else:
-            print("Réponse invalide")
-            continue
-        # Vitesse de convergence selon la précision (graphes avec matplotlib)
-    t = np.linspace(0, 10, 100) # L'axe des abscisses qui représente la précision
-    Xzero = np.array([[0.],[0.],[0.]])
+    input("Appuyer sur entrée pour continuer ...")
 
-    # Les valeurs du vecteur X solution calculées avec la méthode du gradient conjugué
-    x0_conjgrad = [conjgrad_2(A, B, Xzero, imax, 10**(-i))[0] for i in t]
-    x1_conjgrad = [conjgrad_2(A, B, Xzero, imax, 10**(-i))[1] for i in t]
-    x2_conjgrad = [conjgrad_2(A, B, Xzero, imax, 10**(-i))[2] for i in t]
+    print("\n Test avec une matrice aléatoire générée par la fonction matrice_SDP : \n")
+    matrix_size = int(input("Entrer la taille de la matrice A: "))
+    nbr_extra_diag = int(input("Ainsi que le nombre d'elements extra-diagonaux de A: "))
+    curve_nbr = int(input("Veuiller preciser le nombre de courbes à tracer : "))
 
-    # Les valeurs du vecteur X solution calculées avec la fonction solve de la bibliothèque scipy.linalg
-    x0_solve = [X2[0] for i in t]
-    x1_solve = [X2[1] for i in t]
-    x2_solve = [X2[2] for i in t]
+        # Vitesse de convergence absolue
+    t = np.linspace(-5, 0, 50) # L'axe des abscisses qui représente la précision
+    B = sdp.random_vector(matrix_size)
 
-    plt.plot(t, x0_conjgrad, color='red', label="X[0]_conjgrad")
-    plt.plot(t, x1_conjgrad, color='blue', label="X[1]_conjgrad")
-    plt.plot(t, x2_conjgrad, color='green', label="X[2]_conjgrad")
-    plt.plot(t, x0_solve, color='red', ls="dotted", label="X[0]_solve")
-    plt.plot(t, x1_solve, color='blue', ls="dotted", label="X[1]_solve")
-    plt.plot(t, x2_solve, color='green', ls="dotted", label="X[2]_solve")
+    for c_index in range(curve_nbr):
+        A = sdp.matrice_SDP(matrix_size, nbr_extra_diag)
+        #B = sdp.random_vector(matrix_size)
+        Xzero = np.zeros((matrix_size,1))
+        X2 = spl.solve(A, B)
+
+        print("\n A = \n", A)
+        print("\n B = \n", B)
+        print("\n Xzero = \n", Xzero)
+        print("\n X_solve = \n", X2)
+        tab = []
+        for i in t:
+            if (c_index < curve_nbr/2 ):
+                X_diff = conjgrad(A, B, Xzero, imax, 10**(-i)) - X2
+            else:
+                X_diff = conjgrad_precond(A, B, Xzero, imax, 10**(-i)) - X2
+            scalar_product = np.transpose(X_diff).dot(X_diff)[0][0]
+            tab.append(scalar_product)
+        plt.plot(t, tab, label="produit scalaire " + str(c_index + 1))
+        if(c_index >= curve_nbr/2 ):
+            print("scalar_product = ", scalar_product)
+    input("Appuyer sur entrée pour continuer ...")
 
     plt.legend()
-    plt.xlabel("précision")
+    plt.xlabel("précision 10^(-x)")
     plt.show()
 
-#def conjgrad_precond():
+tst_conjgrad()
